9eeb84d3aa94d2c9ad6ed7d023775a2aeedfcbcd
David Balyan <davidbalyan9@gmail.com>
2025-05-22 19:10:48 +0300

    added language optimisations

--- functions.php
+++ functions.php
@@ -35,6 +35,88 @@ function include_language_specific_part($type) {
     include_once($file . '.php');
 }
 
+/**
+ * Get language-specific content and URLs
+ * 
+ * @param string $key The content key to retrieve
+ * @return string The language-specific content
+ */
+function get_language_content($key) {
+    $current_url = $_SERVER['REQUEST_URI'];
+    $lang = '';
+    
+    // Determine current language from URL
+    if (strpos($current_url, '/el/') !== false) {
+        $lang = 'el';
+    } elseif (strpos($current_url, '/ru/') !== false) {
+        $lang = 'ru';
+    }
+    
+    // Language-specific content mapping
+    $content_map = [
+        'home' => [
+            'default' => 'Home',
+            'el' => 'Αρχική',
+            'ru' => 'Главная'
+        ],
+        'more' => [
+            'default' => 'More',
+            'el' => 'Περισσότερο',
+            'ru' => 'Подробнее'
+        ],
+        'book' => [
+            'default' => 'Book',
+            'el' => 'Βιβλίο',
+            'ru' => 'Забронировать'
+        ]
+    ];
+    
+    // Return language-specific content or default if not found
+    return isset($content_map[$key][$lang]) ? $content_map[$key][$lang] : $content_map[$key]['default'];
+}
+
+/**
+ * Get language-specific URL
+ * 
+ * @param string $path The path to append to the language-specific URL
+ * @return string The complete language-specific URL
+ */
+function get_language_url($path = '') {
+    $current_url = $_SERVER['REQUEST_URI'];
+    $lang = '';
+    
+    // Determine current language from URL
+    if (strpos($current_url, '/el/') !== false) {
+        $lang = 'el';
+    } elseif (strpos($current_url, '/ru/') !== false) {
+        $lang = 'ru';
+    }
+    
+    // Build language-specific URL
+    $base_url = get_home_url();
+    if ($lang) {
+        $base_url .= '/' . $lang;
+    }
+    
+    return $base_url . '/' . ltrim($path, '/');
+}
+
+/**
+ * Filter the template hierarchy to use our consolidated template
+ */
+function use_consolidated_template($templates) {
+    $current_url = $_SERVER['REQUEST_URI'];
+    
+    // Check if we're on an activities page
+    if (strpos($current_url, 'activities') !== false) {
+        // Add our consolidated template to the beginning of the template hierarchy
+        array_unshift($templates, 'template-activities-consolidated.php');
+    }
+    
+    return $templates;
+}
+add_filter('page_template_hierarchy', 'use_consolidated_template');
+
 		
 	
 ?>
---
+++ template-activities-consolidated.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Template Name: Activities (Consolidated)
+ * 
+ * A consolidated template for activities that handles all languages
+ */
+
+// Include the appropriate header based on language
+include_language_specific_part('header');
+
+// Get the current language
+$current_url = $_SERVER['REQUEST_URI'];
+$lang = '';
+if (strpos($current_url, '/el/') !== false) {
+    $lang = 'el';
+} elseif (strpos($current_url, '/ru/') !== false) {
+    $lang = 'ru';
+}
+
+// Language-specific content
+$content = [
+    'default' => [
+        'title' => 'Activities',
+        'description' => 'Discover our exciting activities',
+        'book_now' => 'Book Now',
+        'more_info' => 'More Information'
+    ],
+    'el' => [
+        'title' => 'Δραστηριότητες',
+        'description' => 'Ανακαλύψτε τις συναρπαστικές μας δραστηριότητες',
+        'book_now' => 'Κράτηση Τώρα',
+        'more_info' => 'Περισσότερες Πληροφορίες'
+    ],
+    'ru' => [
+        'title' => 'Активности',
+        'description' => 'Откройте для себя наши увлекательные мероприятия',
+        'book_now' => 'Забронировать',
+        'more_info' => 'Подробнее'
+    ]
+];
+
+// Get content for current language or default
+$current_content = isset($content[$lang]) ? $content[$lang] : $content['default'];
+?>
+
+<div class="activities-container">
+    <h1><?php echo esc_html($current_content['title']); ?></h1>
+    <p><?php echo esc_html($current_content['description']); ?></p>
+    
+    <!-- Activity Cards -->
+    <div class="activity-cards">
+        <?php
+        // Example activity data - you would typically get this from WordPress
+        $activities = [
+            [
+                'title' => [
+                    'default' => 'Hiking',
+                    'el' => 'Πεζοπορία',
+                    'ru' => 'Пеший туризм'
+                ],
+                'description' => [
+                    'default' => 'Explore beautiful trails',
+                    'el' => 'Εξερευνήστε όμορφα μονοπάτια',
+                    'ru' => 'Исследуйте красивые тропы'
+                ]
+            ],
+            // Add more activities as needed
+        ];
+        
+        foreach ($activities as $activity) {
+            $activity_title = isset($activity['title'][$lang]) ? $activity['title'][$lang] : $activity['title']['default'];
+            $activity_desc = isset($activity['description'][$lang]) ? $activity['description'][$lang] : $activity['description']['default'];
+            ?>
+            <div class="activity-card">
+                <h2><?php echo esc_html($activity_title); ?></h2>
+                <p><?php echo esc_html($activity_desc); ?></p>
+                <a href="<?php echo esc_url(get_language_url('book')); ?>" class="book-button">
+                    <?php echo esc_html($current_content['book_now']); ?>
+                </a>
+            </div>
+            <?php
+        }
+        ?>
+    </div>
+</div>
+
+<?php
+// Include the appropriate footer based on language
+include_language_specific_part('footer');
+?> 
