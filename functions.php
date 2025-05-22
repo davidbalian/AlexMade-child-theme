9734e93bd9486e4b1c91de804e140768b2ba7747
David Balyan <davidbalyan9@gmail.com>
2025-05-22 20:54:05 +0300

    tried fix

--- functions.php
+++ functions.php
@@ -1,39 +1,37 @@
 <?php 
+// Load parent theme functions
+require_once get_template_directory() . '/functions.php';
 
-    if (function_exists('acf_add_options_page')) {
-
-	    $parent = acf_add_options_page(array(
-	        'page_title' => 'ACF Settings',
-	        'menu_title' => 'Основное',
-	        'icon_url' => 'dashicons-art',
-	    ));
-
-	}
-
-	register_nav_menus(array(
-		'top'    => 'Меню',    
-		'top_el' => 'Меню el',
-		'top_ru' => 'Меню ru',   
-		));	
-
-/**
- * Dynamically includes the correct header or footer based on the current language.
- *
- * @param string $type The type of include ('header' or 'footer').
- */
-function include_language_specific_part($type) {
-    $current_url = $_SERVER['REQUEST_URI'];
-    $file = $type . '-box';
-    
-    // Check if the URL contains language indicators
-    if (strpos($current_url, '/el/') !== false) {
-        $file .= '_el';
-    } elseif (strpos($current_url, '/ru/') !== false) {
-        $file .= '_ru';
+// If the parent theme's function doesn't exist, define it here
+if (!function_exists('include_language_specific_part')) {
+    function include_language_specific_part($type) {
+        $current_url = $_SERVER['REQUEST_URI'];
+        $file = $type . '-box';
+        
+        // Check if the URL contains language indicators
+        if (strpos($current_url, '/el/') !== false) {
+            $file .= '_el';
+        } elseif (strpos($current_url, '/ru/') !== false) {
+            $file .= '_ru';
+        }
+        
+        include_once(get_stylesheet_directory() . '/' . $file . '.php');
     }
-    
-    include_once($file . '.php');
 }
+
+if (function_exists('acf_add_options_page')) {
+    $parent = acf_add_options_page(array(
+        'page_title' => 'ACF Settings',
+        'menu_title' => 'Основное',
+        'icon_url' => 'dashicons-art',
+    ));
+}
+
+register_nav_menus(array(
+    'top'    => 'Меню',    
+    'top_el' => 'Меню el',
+    'top_ru' => 'Меню ru',   
+));
 
 /**
  * Get language-specific content and URLs
